a889fe5fe8bd79b7226e2e4989195bf71e0d6a58
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2019-06-09 21:55:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
   function lol() {
     client.guilds.get('398020677178753024').roles.find("name", "Rainbow").setColor("RANDOM");
   }
-  setInterval(lol, 7000);
+  setInterval(lol, 4000);
 })
 
 
